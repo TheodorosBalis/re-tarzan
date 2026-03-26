a395f5e915fe30f0a984e84194e6f5c571963720
TheodorosBalis <tbtbalis@gmail.com>
2026-03-26 10:56:54 +0200

    Updated

--- Menu/ProcessPauseOrOptionsMenus.c
+++ Menu/ProcessPauseOrOptionsMenus.c
@@ -6,7 +6,7 @@ void ProcessPauseOrOptionsMenus(void)
   int iVar1;
   code *pcVar2;
   
-  if (g_OptionsMenuStatus == 0) {
+  if (g_PauseMenuStatus == 0) {
     g_ActiveOptionsMenuHandler = (code *)0x0;
     pcVar2 = (code *)ResolveOptionsMenuHandlerById(g_PendingOptionsMenuId);
     if (pcVar2 != (code *)0xffffffff) {
@@ -15,7 +15,7 @@ void ProcessPauseOrOptionsMenus(void)
     g_ActiveOptionsMenuId = g_PendingOptionsMenuId;
     g_PendingOptionsMenuId = 0;
     if (g_ActiveOptionsMenuHandler != (code *)0x0) {
-      g_OptionsMenuStatus = 2;
+      g_PauseMenuStatus = 2;
       InitSoundAndNewGameStateFile(0xac);
       PlaySoundID(0xac,100,0,0,0x3fb33333);
       iVar1 = g_BrightnessPercent;
@@ -25,25 +25,25 @@ void ProcessPauseOrOptionsMenus(void)
       FillArrayOfNumbers255(iVar1);
     }
   }
-  else if (g_OptionsMenuStatus == 2) {
+  else if (g_PauseMenuStatus == 2) {
     if ((g_ActiveOptionsMenuHandler == (code *)0x0) ||
        (iVar1 = (*g_ActiveOptionsMenuHandler)(), iVar1 == 0)) {
       bIsPaused = 1;
-      g_OptionsMenuStatus = 3;
+      g_PauseMenuStatus = 3;
       return;
     }
   }
-  else if ((g_OptionsMenuStatus == 3) && (iVar1 = DrawOptionsMenu(0,0xffffffff), iVar1 == 0)) {
+  else if ((g_PauseMenuStatus == 3) && (iVar1 = DrawOptionsMenu(0,0xffffffff), iVar1 == 0)) {
     if (g_ReturnOptionsMenuId != 0) {
       SetOptionsMenu(g_ReturnOptionsMenuId);
     }
-    g_OptionsMenuStatus = 0;
+    g_PauseMenuStatus = 0;
     if (g_PendingOptionsMenuId == 0) {
       FillArrayOfNumbers255(g_BrightnessPercent);
     }
     CaptureKInputsInBuffer();
   }
-  if ((g_OptionsMenuStatus == 0) && (g_PendingOptionsMenuId == 0)) {
+  if ((g_PauseMenuStatus == 0) && (g_PendingOptionsMenuId == 0)) {
     bIsPaused = 0;
     return;
   }
