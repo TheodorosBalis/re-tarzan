4c8c931e338828410c90148ecd6bc3ea07abe273
TheodorosBalis <tbtbalis@gmail.com>
2026-03-28 05:46:04 +0200

    Updated

---
+++ Menu/CMenu__StartGame.c
@@ -0,0 +1,15 @@
+// 0x452FD0
+void CMenu__StartGame(void)
+
+{
+  InitiateFade(2,0x100);
+  g_LevelTransitionContext._3_1_ =
+       (&g_LevelTransitionTypeBySlot)[g_LevelTransitionContext._1_1_ * 2];
+  g_LoadTransitionFlags._0_2_ = 0;
+  gCurrentCoinsCollected = 0;
+  g_SelectedLevelIndex = 0;
+  InitCinematic_stub(&DAT_00c46070);
+  InitiateFade(2,0x100);
+  MenuState = 2;
+  return;
+}
