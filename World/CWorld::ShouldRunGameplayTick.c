9ed14cfc3e2c9ce66d1d973703df42c30573e77a
TheodorosBalis <tbtbalis@gmail.com>
2026-04-19 23:13:00 +0300

    CWorld added

---
+++ World/CWorld::ShouldRunGameplayTick.c
@@ -0,0 +1,8 @@
+uint CWorld::ShouldRunGameplayTick(void)
+
+{
+  if (((short)g_GameplayStateFlags != 0) && (g_PlayerDamageFlashTicks != 0)) {
+    *(byte *)(g_PlayerSceneEntryData + 3) = *(byte *)(g_PlayerSceneEntryData + 3) | 0x80;
+  }
+  return ~g_GameplayStateFlags >> 0xb & 1;
+}
