ae125b00c072fe5b91cdfd400c6d95ef0ed7cc50
TheodorosBalis <tbtbalis@gmail.com>
2026-04-23 15:37:42 +0300

    Updated

--- Rendering/RequestGraphicsModeChange.c
+++ Rendering/RequestGraphicsModeChange.c
@@ -1,18 +1,18 @@
 // Address: 0x0049C7E0
 
-undefined4 SetActiveBitmapSlot(int param_1)
+undefined4 RequestGraphicsModeChange(int param_1)
 
 {
   if (param_1 != 7) {
     if ((param_1 < 1) || (5 < param_1)) {
       param_1 = 5;
     }
-    DAT_00515de0 = -param_1;
+    g_RequestedGraphicsMode = -param_1;
     if ((&DAT_0072297c)[param_1] != 0) {
       return 0;
     }
   }
-  DAT_00515de0 = param_1;
+  g_RequestedGraphicsMode = param_1;
   return 1;
 }
 
