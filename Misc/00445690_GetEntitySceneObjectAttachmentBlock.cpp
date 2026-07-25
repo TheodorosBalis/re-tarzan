d6ed6e246a79db12293d20a98401c19addb87150
TheodorosBalis <tbtbalis@gmail.com>
2026-07-25 03:43:53 +0300

    Update 00445690_GetEntitySceneObjectAttachmentBlock.cpp

--- Misc/00445690_GetEntitySceneObjectAttachmentBlock.cpp
+++ Misc/00445690_GetEntitySceneObjectAttachmentBlock.cpp
@@ -26,8 +26,7 @@ int GetEntitySceneObjectAttachmentBlock(int arg1)
   value2 = value & 0xfff;
   value3 = value & 0x7000;
   if ((value & 0x7000) == 0) {
-    entry2 = ResolveSceneContextPackedRelativeBlock
-                      (*(int *)(entry + 8) + 0x10 + value2 * 4 + entry,entry2);
+    entry2 = ResolveSceneContextPackedRelativeBlock(*(int *)(entry + 8) + 0x10 + value2 * 4 + entry,entry2);
     return entry2;
   }
   if (value3 == 0x2000) {
@@ -37,9 +36,7 @@ int GetEntitySceneObjectAttachmentBlock(int arg1)
     }
   }
   else if (value3 == 0x5000) {
-    entry2 = ResolveSceneAssetRelativePointerEntry
-                      (*(int *)(*(int *)(entry2 + 0xc) + 0x10) + value2 * 8 + 0x14 +
-                       *(int *)(entry2 + 0xc));
+    entry2 = ResolveSceneAssetRelativePointerEntry(*(int *)(*(int *)(entry2 + 0xc) + 0x10) + value2 * 8 + 0x14 + *(int *)(entry2 + 0xc));
     if (entry2 == 0) {
       return 0;
     }
